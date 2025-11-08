f5cc04fdfe9a961e2420acab55b7f62b931fb4ee
Jeremy Vinding <github@jeremyvinding.com>
2025-11-08 11:46:23 -0700

    why can't i get leds working on split keyboards?!?!

--- keyboards/controllerworks/mini42/keymaps/jvinding_mini42_miryoku/rules.mk
+++ keyboards/controllerworks/mini42/keymaps/jvinding_mini42_miryoku/rules.mk
@@ -2,7 +2,7 @@ TAP_DANCE_ENABLE = yes
 CAPS_WORD_ENABLE = yes
 RGBLIGHT_ENABLE = no
 BACKLIGHT_ENABLE = no
-RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_ENABLE = no
 
 # Include additional source files
 SRC += tap_dance.c
