089db77edf4cb7ecbfaddf2cfb60505b2fce9f74
Jeremy Vinding <github@jeremyvinding.com>
2025-11-08 17:04:35 -0700

    fix cut/copy/paste, undo/redo, and caps word/caps lock

--- keyboards/controllerworks/city42/keymaps/jvinding_city42_miryoku/rules.mk
+++ keyboards/controllerworks/city42/keymaps/jvinding_city42_miryoku/rules.mk
@@ -1,8 +1,12 @@
 TAP_DANCE_ENABLE = yes
-CAPS_WORD_ENABLE = yes
+KEY_OVERRIDE_ENABLE = yes
+
 RGBLIGHT_ENABLE = no
 BACKLIGHT_ENABLE = no
 RGB_MATRIX_ENABLE = yes
+
+CAPS_WORD_ENABLE = yes
+CAPS_WORD_INVERT_ON_SHIFT = yes
 
 # Include additional source files
 SRC += tap_dance.c
--- keyboards/controllerworks/mini42/keymaps/jvinding_mini42_miryoku/config.h
+++ keyboards/controllerworks/mini42/keymaps/jvinding_mini42_miryoku/config.h
@@ -5,10 +5,10 @@
 #define CHORDAL_HOLD
 
 // Split keyboard settings
-#define SPLIT_TRANSPORT_MIRROR
-#define SPLIT_LAYER_STATE_ENABLE
-#define SPLIT_LED_STATE_ENABLE
-#define SPLIT_MODS_ENABLE
+// #define SPLIT_TRANSPORT_MIRROR
+// #define SPLIT_LAYER_STATE_ENABLE
+// #define SPLIT_LED_STATE_ENABLE
+// #define SPLIT_MODS_ENABLE
 
 // Disable features we don't need
 #define NO_ACTION_ONESHOT
@@ -16,9 +16,8 @@
 
 // Enable RGB Matrix - disable all effects and animations
 #ifdef RGB_MATRIX_ENABLE
-#    define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_NONE
-#    define RGB_MATRIX_DISABLE_KEYCODES  // Disable RGB keycodes to prevent mode changes
-#    define RGB_MATRIX_DISABLE_TIMEOUT 0  // Disable timeout
+// #    define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_NONE
+// #    define RGB_MATRIX_DISABLE_KEYCODES  // Disable RGB keycodes to prevent mode changes
+// #    define RGB_MATRIX_DISABLE_TIMEOUT 0  // Disable timeout
 #    define RGB_MATRIX_DISABLE_WHEN_USB_SUSPENDED  // Disable when suspended
 #endif
-
